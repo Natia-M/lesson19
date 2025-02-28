0989f311ac12d456b0df74a8391807b23b9da69c
NatiaMinasiani <natiaminasiani@gmail.com>
2025-03-01 01:03:32 +0300

    completed js file

---
+++ main.js
@@ -0,0 +1,54 @@
+window.onload = function () {
+  let img = document.createElement("img");
+  img.src =
+    "https://fastly.picsum.photos/id/180/2000/1600.jpg?hmac=dr3QTXUHbgPDvKgE9s2UjynoVFsZdxoLI3cxK8YZd9A";
+  img.alt = "სურათი";
+  img.style.width = "500px";
+
+  document.body.appendChild(img);
+};
+//4. დავალება
+const characters = [
+  {
+    first_name: "Harry",
+    last_name: "Potter",
+    house: "Gryffindor",
+    image: "images/img1.png",
+    actor: "Daniel Radcliffe",
+  },
+  {
+    first_name: "Hermione",
+    last_name: "Granger",
+    house: "Gryffindor",
+    image: "images/img2.png",
+    actor: "Emma Watson",
+  },
+  {
+    first_name: "Ron",
+    last_name: "Weasley",
+    house: "Gryffindor",
+    image: "images/img3.png",
+    actor: "Rupert Grint",
+  },
+  {
+    first_name: "Draco",
+    last_name: "Malfoy",
+    house: "Slytherin",
+    image: "images/img4.png",
+    actor: "Tom Felton",
+  },
+];
+const charactersHTML = characters
+
+  .map(
+    (character) => `
+    <div class="character-card ${character.house.toLowerCase()}">
+        <img src="${character.image}" alt="${character.first_name}">
+        <h2>${character.first_name} ${character.last_name}</h2>
+        <p>House: <strong>${character.house}</strong></p>
+        <p>Actor: <strong>${character.actor}</strong></p>
+    </div>
+`
+  )
+  .join("");
+document.getElementById("characters-list").innerHTML = charactersHTML;
